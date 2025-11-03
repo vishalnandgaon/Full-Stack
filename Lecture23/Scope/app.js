1c93b85b50beb6b396c62ac02eec5a6af0fcee11
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-11-03 12:22:45 +0530

    Scope of a variable

---
+++ Lecture23/Scope/app.js
@@ -0,0 +1,10 @@
+var b=10;
+
+function write(){
+    console.log(b);
+}
+
+write();
+
+// scope --. local scope and global scope 
+// jab bhi global context call hota h , toh har baar global object bhi banta h 
